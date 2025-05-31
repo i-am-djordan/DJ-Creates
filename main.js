25b5399741ce93b97a721b5fbfd8a0977a8e9d77
DJ <jordan.destiny05@gmail.com>
2025-05-31 17:55:56 -0400

    Implement code changes to enhance functionality and improve performance

--- main.js
+++ main.js
@@ -4,21 +4,24 @@ fetch("header.html")
     document.getElementById("header").innerHTML = data;
 
     const toggleButton = document.getElementById("menu-toggle");
-    const closeButton = document.getElementById("close-menu");
     const navMenu = document.getElementById("nav-menu");
     const overlay = document.getElementById("overlay");
     const content = document.getElementById("page-content");
+
+    // Move sidebarLinks selection and event attachment after header is loaded
+    const sidebarLinks = document.querySelectorAll('.sidebar a');
+    sidebarLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navMenu.classList.remove('show');
+            content.classList.remove('shifted');
+            overlay.classList.remove('show');
+        });
+    });
 
     toggleButton.addEventListener("click", () => {
       navMenu.classList.toggle("show");
       overlay.classList.toggle("show");
       content.classList.toggle("shifted");
-
-    });
-
-    closeButton.addEventListener("click", () => {
-      navMenu.classList.toggle("hide");
-      overlay.classList.toggle("hide");
     });
 
     overlay.addEventListener("click", () => {
